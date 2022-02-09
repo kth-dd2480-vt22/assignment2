a8282fdd31eb1e2295a3fd57faee95c75195e99a
Edwin <edwinso@kth.se>
2022-02-09 19:17:10 +0100

    fix: replace javax with jakarta

--- src/main/java/karmosin/WebHookHandler.java
+++ src/main/java/karmosin/WebHookHandler.java
@@ -1,12 +1,12 @@
 package karmosin;
 
 import java.io.IOException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.eclipse.jetty.server.Request;
 
-import javax.servlet.ServletException;
+import jakarta.servlet.ServletException;
 
 public interface WebHookHandler {
     /**
