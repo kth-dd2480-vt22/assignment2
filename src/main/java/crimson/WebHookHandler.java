2e420ea634eb78a3622c75d3154863c8f7f091af
Siyao Liu <39266859+641bill@users.noreply.github.com>
2022-02-10 18:14:26 +0100

    feat: Implements the parseEvent function so that it can parse a valid push event and return the corresponding CI Job. (#26)
    * Modifies build.gradle to specify the JDK version to be 11
    Fixes #11
    
    * feat: Implements the parseEvent function so that it can parse a valid push event and return the corresponding CI Job.
    A valid test is also present to test parsing a valid push event. Invalid and "GET" requests or other events might need to be handled next.
    build.gradle is modified correspondingly so that it can successfully build with java 8 by removing the dependency of java 11 otherwise "invalid source release: 11" is throwed, jetty version is modified along with it, and necessary dependency of json, springframework added.

---
+++ src/main/java/crimson/WebHookHandler.java
@@ -0,0 +1,62 @@
+package crimson;
+import karmosin.*;
+import org.eclipse.jetty.server.Request;
+import org.json.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URL;
+import java.util.stream.Collectors;
+
+public class WebHookHandler implements karmosin.WebHookHandler {
+
+    /**
+     * parseEvent responsible for parsing an event from the Webhook and create an
+     * ContinuousIntegrationJob object.
+     *
+     * @param target
+     * @param baseRequest
+     * @param request
+     * @param response
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public ContinuousIntegrationJob parseEvent(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        ContinuousIntegrationJob job = new ContinuousIntegrationJob();
+        if (request.getMethod().equals("POST")) {
+            JSONObject json = new JSONObject(request.getReader().lines().collect(Collectors.joining()));
+            String gitRef = json.getString("ref");
+            String url = json.getJSONObject("repository").getString("url");
+            URL repoUrl = new URL(url);
+            JSONArray commits = json.getJSONArray("commits");
+            JSONObject commit = commits.getJSONObject(0);
+            String commitHash = commit.getString("id");
+            String pusherName = commit.getJSONObject("author").getString("name");
+            String pusherEmail = commit.getJSONObject("author").getString("email");
+            job.gitRefs = gitRef;
+            job.repoGitUrl = repoUrl;
+            job.commitHash = commitHash;
+            job.pusherName = pusherName;
+            job.pusherEmail = pusherEmail;
+        }
+        return job;
+    }
+
+    /**
+     * responseEvent() response to an WebHook event (e.g. tell the status)
+     *
+     * @param target
+     * @param baseRequest
+     * @param request
+     * @param response
+     * @param continuousIntegrationJob
+     * @throws Exception
+     */
+    @Override
+    public void responseEvent(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response, ContinuousIntegrationJob continuousIntegrationJob) throws IOException, ServletException {
+
+    }
+}
