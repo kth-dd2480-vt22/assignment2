bd014256aa91bb349a75fddf4dc2e66ac5aae1ac
Edwin <edwinso@kth.se>
2022-02-10 17:20:01 +0100

    add job ID for each CI Job

--- src/main/java/crimson/ContinuousIntegrationHandler.java
+++ src/main/java/crimson/ContinuousIntegrationHandler.java
@@ -43,7 +43,7 @@ public class ContinuousIntegrationHandler extends AbstractHandler {
 
         Runnable r = new Runnable() {
             public void run() {
-                String jobName = "";
+                String jobName = job.jobID;
 
                 try {
                     String tmpdir = Files.createTempDirectory("ci_job").toFile().getAbsolutePath();
